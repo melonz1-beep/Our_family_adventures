d855336a6c042de29255eff3a495462f736356aa
melonz1-beep <mlehr1211@gmail.com>
2026-06-25 13:52:09 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='project-lighthouse-build-0009-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./firebase.js','./manifest.json','./assets/lighthouse-home.png','./assets/lighthouse-scene.png','./icons/icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
