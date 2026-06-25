2fe810bfce252abe117836ba329229a17714859f
melonz1-beep <mlehr1211@gmail.com>
2026-06-25 08:39:08 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='project-lighthouse-build-0007';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./lighthouse-home.png','./lighthouse-scene.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
