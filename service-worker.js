ea2da42c0b4cb15f715a86fc28cec7e7e69a7ca7
melonz1-beep <mlehr1211@gmail.com>
2026-06-26 06:38:02 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='project-lighthouse-v3-0';
+const ASSETS=['./','index.html','style.css','app.js','firebase-config.js','manifest.json','assets/lighthouse-home.png','assets/lighthouse-scene.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
