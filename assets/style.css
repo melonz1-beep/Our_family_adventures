74059d5cc0199e7e21d8ee775719a3cf476cd872
melonz1-beep <mlehr1211@gmail.com>
2026-06-26 06:27:25 -0400

    Delete assets directory

--- assets/style.css
+++
@@ -1,3 +0,0 @@
-# Our Family Adventures - Build 3.7
-
-Scrapbook + Memories Polish. Upload everything inside this folder to GitHub. Memories can be saved separately, added to scrapbook layouts, printed, downloaded, and shared. Contact buttons use the device email/text/call apps.
