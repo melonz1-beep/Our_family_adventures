afd0655eeb7ce5b620a26f0918f19e26a4a7c652
melonz1-beep <mlehr1211@gmail.com>
2026-06-26 06:26:06 -0400

    Add files via upload

---
+++ assets/style.css
@@ -0,0 +1,3 @@
+# Our Family Adventures - Build 3.7
+
+Scrapbook + Memories Polish. Upload everything inside this folder to GitHub. Memories can be saved separately, added to scrapbook layouts, printed, downloaded, and shared. Contact buttons use the device email/text/call apps.
