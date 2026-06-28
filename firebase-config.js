93c9081f379f14f63724fe748f377b7569007626
melonz1-beep <mlehr1211@gmail.com>
2026-06-28 14:13:08 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,14 @@
 // 6) Keep window.OFA_FIREBASE_ENABLED = true.
 
 window.firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
+  apiKey: "AIzaSyCr4mRHzubFblqpdiaBB4lYWwF9TPc4C2g",
   authDomain: "our-family-adventures.firebaseapp.com",
+  databaseURL: "https://our-family-adventures-default-rtdb.firebaseio.com",
   projectId: "our-family-adventures",
-  storageBucket: "our-family-adventures.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  storageBucket: "our-family-adventures.firebasestorage.app",
+  messagingSenderId: "283204446220",
+  appId: "1:283204446220:web:6dfc7570fbff76dc6fa629",
+  measurementId: "G-DDRV34JC2Z"
 };
 
 window.OFA_FIREBASE_ENABLED = true;
