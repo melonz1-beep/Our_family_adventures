90ebe69921dd56719cbb441ac9226a5f61b3a412
melonz1-beep <mlehr1211@gmail.com>
2026-07-20 09:41:36 -0400

    Security 10.3.9: update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-/* Our Family Adventures 10.3.8 PWA cache worker. Push is handled by firebase-messaging-sw.js. */
-const CACHE='ofa-10-3-8';
-const OFFLINE='./index.html?v=10.3.8';
-const CORE=['./','./index.html?v=10.3.8','./404.html','./style.css?v=10.3.8','./scrapbook-studio-2.css?v=10.3.8','./app.js?v=10.3.8','./scrapbook-studio-2.js?v=10.3.8','./manifest.json?v=10.3.8','./firebase-config.js?v=10.3.8','./assets/lighthouse-home.png','./icons/icon-192.png','./icons/icon-512.png'];
+/* Our Family Adventures 10.3.9 PWA cache worker. Push is handled by firebase-messaging-sw.js. */
+const CACHE='ofa-10-3-9';
+const OFFLINE='./index.html?v=10.3.9';
+const CORE=['./','./index.html?v=10.3.9','./404.html','./style.css?v=10.3.9','./scrapbook-studio-2.css?v=10.3.9','./app.js?v=10.3.9','./scrapbook-studio-2.js?v=10.3.9','./manifest.json?v=10.3.9','./firebase-config.js?v=10.3.9','./assets/lighthouse-home.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
